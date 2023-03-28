0d55bd380136c3f5c22028c65285463d7234e0cd
LEONCIDES FERREIRA DA SILVA NETO <leoncides.9916@aluno.pr.senac.br>
2023-03-28 14:50:45 -0300

    atualização

--- model/Vermifugo.php
+++ model/Vermifugo.php
@@ -2,9 +2,9 @@
 require_once './persistence/Banco.php'; 
 
 
-function aplicarVermifugoVaca($ID_verm,$Nome_verm,$Marca_verm,$Lote_verm,$Fabricação_verm,$Validade_verm,$aplicacao_verm,$proximaapli_verm){ 
+function aplicarVermifugo($ID_vermifugo,$nome_verm,$marca_verm,$lote_verm,$fabricacao_verm,$validade_verm,$aplicacao_verm,$proxima_verm){ 
 	$banco=new Banco();
-	$sql="insert into vermifugo_vac values($ID_verm,'$Nome_verm','$Marca_verm','$Lote_verm','$Fabricação_verm','$Validade_verm','$aplicacao_verm','$proximaapli_verm')";
+	$sql="insert into vermifugo values($ID_vermifugo,'$nome_verm','$marca_verm','$lote_verm','$fabricacao_verm','$validade_verm','$aplicacao_verm','$proxima_verm')";
 	$resp=$banco->executar($sql);
 	if($resp){
 		return true;
@@ -14,9 +14,9 @@ function aplicarVermifugoVaca($ID_verm,$Nome_verm,$Marca_verm,$Lote_verm,$Fabric
 }
 
 
-function aplicarVermifugoCavalo($ID_verm,$Nome_verm,$Marca_verm,$Lote_verm,$Fabricação_verm,$Validade_verm,$aplicacao_verm,$proximaapli_verm){ 
+function aplicarVermifugo($ID_vermifugo,$nome_verm,$marca_verm,$lote_verm,$fabricacao_verm,$validade_verm,$aplicacao_verm,$proxima_verm){ 
 	$banco=new Banco();
-	$sql="insert into vermifugo_cav values($ID_verm,'$Nome_verm','$Marca_verm','$Lote_verm','$Fabricação_verm','$Validade_verm','$aplicacao_verm','$proximaapli_verm')";
+	$sql="insert into vermifugo values($ID_vermifugo,'$nome_verm','$marca_verm','$lote_verm','$fabricacao_verm','$validade_verm','$aplicacao_verm','$proxima_verm')";
 	$resp=$banco->executar($sql);
 	if($resp){
 		return true;
@@ -25,44 +25,10 @@ function aplicarVermifugoCavalo($ID_verm,$Nome_verm,$Marca_verm,$Lote_verm,$Fabr
 	}
 }
 
-function aplicarVermifugoOvelha($ID_verm,$Nome_verm,$Marca_verm,$Lote_verm,$Fabricação_verm,$Validade_verm,$aplicacao_verm,$proximaapli_verm){ 
-	$banco=new Banco();
-	$sql="insert into vermifugo_ovl values($ID_verm,'$Nome_verm','$Marca_verm','$Lote_verm','$Fabricação_verm','$Validade_verm','$aplicacao_verm','$proximaapli_verm')";
-	$resp=$banco->executar($sql);
-	if($resp){
-		return true;
-	}else{
-		return false;
-	}
-}
 
- function verificarVermifugoVaca($vermifugo_vac){
+ function verificarVermifugo($vermifugo){
 	$banco=new Banco();
-	$sql="select proximaapli_verm from  vermifugo_vac='$vermifugo_vac'";
-	$consulta=$banco->consultar($sql);
-	if (!$consulta) {
-		return false;
-	}else{
-		return $consulta;
-	}
-} 
-
-//	ALERTA PARA VERMIFUGAÇÃO
-
-function verificarVermifugoCavalo($vermifugo_cav){
-	$banco=new Banco();
-	$sql="select proximaapli_verm from  vermifugo_cav='$vermifugo_cav'";
-	$consulta=$banco->consultar($sql);
-	if (!$consulta) {
-		return false;
-	}else{
-		return $consulta;
-	}
-} 
-
-function verificarVermifugoOvelha($vermifugo_ovl){
-	$banco=new Banco();
-	$sql="select proximaapli_verm from  vermifugo_ovl='$vermifugo_ovl'";
+	$sql="select proxima_verm from  vermifugoproxima_verm='$vermifugo'";
 	$consulta=$banco->consultar($sql);
 	if (!$consulta) {
 		return false;
