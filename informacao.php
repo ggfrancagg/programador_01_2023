71703bd503e4b3d47e66a7c29fb145424c8e118f
PRISCILA SIMON <simon.2969@aluno.pr.senac.br>
2023-03-30 15:57:39 -0300

    atualização css e pág de infomações

--- informacao.php
+++ informacao.php
@@ -1,24 +1,25 @@
 <?php require_once 'cabecalho.php' ?>
 
 <h4> Informações e links uteis</h4>
-<br/><br/>
+<hr>
 <h2 id="info"> &#128137; Esquema Vacinal - Bovinos, Equinos e Ovinos &#128137;</h2>
 <br/>
 <h2 id="informa">Bovinos &#128046;</h2>
-<ol id="curio">Vacinas obrigatórias:
-	<li>Febre Aftosa - anual</li>
+<br/>
+<ol id="curio"><h2 id="subt">Vacinas obrigatórias:</h2>
+	<li>Febre Aftosa - anual.</li>
 	<li>B19: Brucelose - 3 a 8 meses de idade, reforço após 7 anos.</li>
 	<li>Raiva: anual (obrigatória dependendo de ocorrências).</li>
 </ol>
-<ol id="curio">Outras Vacinas:
+<ol id="curio"> <h2 id="subt">Outras Vacinas:</h2>
 	<li>Clostridioses - duas doses iniciais com intervalo de 30 dias, após isso anual.</li>
-	<li>IBR/BVD: semestral (animais em reprodução)</li>
-	<li>Leptospirose: semestral (animais em reprodução)</li>
-	<li>Mastite: bimestral (vacas em lactação)</li>
+	<li>IBR/BVD: semestral (animais em reprodução).</li>
+	<li>Leptospirose: semestral (animais em reprodução).</li>
+	<li>Mastite: bimestral (vacas em lactação).</li>
 	<li>Pneumoenterite: dose única (gestante: 7o mês de gestação e bezerros: 1 mês após
 	nascimento).</li>
 </ol>
-<ol id="curio">Exames:
+<ol id="curio"> <h2 id="subt">Exames:</h2>
 	<li>Tuberculose: semestral.</li>
 	<li>Brucelose: semestral.</li>
 	<li>Teste Matite: semanal.</li>
@@ -27,28 +28,31 @@
 
 <br/>
 <h2 id="informa">Equinos &#128052;</h2>
-	<ol id="curio">Vacinas:
-		<li>Tétano: anual (primeira dose a partir de 4 meses)</li>
-		<li>Influenza: anual (primeira dose a partir de 4 meses)</li>
-		<li>Encefalomielite: anual (primeira dose a partir de 4 meses)</li>
-		<li>Herpes Vírus: anual</li>
-		<li>Raiva: anual</li>
-		<li>Garrotilho: semestral</li>
+<br/>
+	<ol id="curio"><h2 id="subt">Vacinas:</h2>
+		<li>Tétano: anual (primeira dose a partir de 4 meses).</li>
+		<li>Influenza: anual (primeira dose a partir de 4 meses).</li>
+		<li>Encefalomielite: anual (primeira dose a partir de 4 meses).</li>
+		<li>Herpes Vírus: anual.</li>
+		<li>Raiva: anual.</li>
+		<li>Garrotilho: semestral.</li>
 	</ol>
 
 <h2 id="informa">Ovinos &#128017;</h2>
-	<ol id="curio">Vacinas:
-		<li>Raiva: anual (a partir de 4 meses)</li>
-		<li>Clostridiose: anual (a partir de 2 meses e gestantes 3 semanas antes do parto)</li>
-		<li>Linfadenite caseosa: anual (3 meses com reforço 30 dias após a primeira dose)</li>
+<br/>
+	<ol id="curio"><h2 id="subt">Vacinas:</h2>
+		<li>Raiva: anual (a partir de 4 meses).</li>
+		<li>Clostridiose: anual (a partir de 2 meses e gestantes 3 semanas antes do parto).</li>
+		<li>Linfadenite caseosa: anual (3 meses com reforço 30 dias após a primeira dose).</li>
 	</ol>
-	<p id="obs">OBS: febre aftosa para ovinos somente com autorização do Ministério da Agricultura.</p>
+	<p id="obs">&#10071; ATENÇÃO:  a vacina para febre aftosa para ovinos somente com autorização do Ministério da Agricultura.</p>
 
 
 <form id="linkes">
-	<h2 id="informa">Links Úteis</h2>
+	<h2 id="informa">&#128668; Links Úteis &#128668;</h2>
+	<hr id="cada">
 <p><a href="http://www.coamo.com.br/jornalcoamo/janfev07/pecuaria.html">Coamo</a></p>
-<p><a href="https://www.milkpoint.com.br/img/artigo/conteudo/102038/"></a>Milkpoint</p>
+<p><a href="https://www.milkpoint.com.br/img/artigo/conteudo/102038/">Milkpoint</a></p>
 <p><a href="https://www.educapoint.com.br/blog/pecuaria-geral/vacinas-obrigatorias-bovinos/">Educapoint</a></p>
 <p><a href="https://dicas.boisaude.com.br/calendario-sanitario-para-bovinos/">Dicas Boisaude</a></p>
 <p><a href="https://www.ourofinosaudeanimal.com/perguntas-frequentes/equinos/qual-e-o-calendario-va
