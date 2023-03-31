5fe4af837ec5df51333e05c3a975aa0badcc5cac
PRISCILA SIMON <simon.2969@aluno.pr.senac.br>
2023-03-31 16:42:49 -0300

    informaçãoVerm add

--- informacaoVerm.php
+++ informacaoVerm.php
@@ -4,5 +4,30 @@
 
 <h4> Informações e links uteis</h4>
 <hr>
-<h2 id="info"> &#128137; Esquema Vacinal - Bovinos, Equinos e Ovinos &#128137;</h2>
+<h2 id="info"> &#128221; Outros cuidados &#128221;</h2>
 <br/>
+<ol id="curio"><h2 id="subt">&#128014; Casqueamento: &#128014;</h2>
+	<li>Indicação a partir de 90 dias de vida. </li>
+	<li>Pode ser realizado de 60 em 60 dias, observando as condições dos animais.</li>
+	<li>Sempre observando como está a saúde do casco do animal, cuidando para não exagerar e machucar.</li>
+</ol>
+<ol id="curio"> <h2 id="subt">&#128138; Vermifugação &#128138;</h2>
+	<li>Recomendação de 60 em 60 dias para evitar a proliferação.</li>
+	<li>Contudo é necessário observar como está a saúde do animal e alertas da região para cuidados diversos.</li>
+</ol>
+
+
+
+
+<form id="linkes">
+	<h2 id="informa">&#128668; Links Úteis &#128668;</h2>
+	<hr id="cada">
+<p><a href="https://www.nobrenutricaoanimal.com.br/blog/importancia-do-casqueamento/">Casqueamento</a></p>
+<p><a href="https://www.cptcursospresenciais.com.br/blog/casqueamento-preventivo-quando-realizar/">Técnicas de Casqueamento</a></p>
+<p><a href="https://www.fundacaoroge.org.br/blog/10-dicas-para-o-controle-de-verminoses-em-bovinos-no-periodo-seco">Dicas sobre Vermifugação</a></p>
+<p><a href="https://cooperativa.coop.br/qual-a-importancia-da-vermifugacao/">Vermifugação</a></p>
+<p><a href="https://blog.prodap.com.br/parasitas-em-bovinos/">Parasitas</a></p>
+</form>
+
+</body>
+</html>
