93ab0b6dfa178b05632a38b1190d1cc091df5bf1
LEONCIDES FERREIRA DA SILVA NETO <leoncides.9916@aluno.pr.senac.br>
2023-04-03 15:38:51 -0300

    a++

--- buscarbovino.php
+++ buscarbovino.php
@@ -5,13 +5,10 @@
 	<h1>Buscar</h1>
 	
 	<p><fieldset>
-<<<<<<< HEAD
-		<legend>Buscar Bovino:</legend>
-			<p><input id="busca" type="search" name="buscar" placeholder="Nome, ID ou raça" required> &#128046;</p>
-=======
+
 		<legend>Buscar Bovino: </legend>
 			<p><input id="busca" type="search" name="tipo" placeholder="Nome, ID ou raça" required> &#128017;</p>
->>>>>>> 5cc9872535106ab98dfe54ac86b22e7ae89b2e77
+
 		
 		</fieldset>
 </p>
@@ -37,20 +34,7 @@
 		if (!$consulta) {
 			echo "<h5>Nenhum bovino correspondente</h5>";
 		}else{
-<<<<<<< HEAD
-			echo "<table>";
-			echo "<tr class='alt'>";
-			echo "<th>Código</th>";
-			echo "<th>Nome</th>";
-			echo "<th>Raça</th>";
-			echo "<th>Peso</th>";
-			echo "<th>Data de nascimento</th>";
-			echo "<th>Raça da Mãe</th>";
-			echo "<th>Raça do Pai</th>";
-			echo "<th>Altura</th>";
-			echo "<th>Sexo</th>";
-			echo "</tr>";
-=======
+
 			echo "<table id='listarbicho'>";
 		echo "<tr>";
 		echo "<th class='ident'> Identificação </th>";
@@ -62,7 +46,6 @@
 		echo "<th class='raça'> Raça da Pai </th>";
 		echo "<th class='alt'> Altura </th>";	
 		echo "<th class='peso'> Peso </th>";
->>>>>>> 5cc9872535106ab98dfe54ac86b22e7ae89b2e77
 
 
 
@@ -71,18 +54,13 @@ while ($linha=$consulta->fetch_assoc()) {
 			echo "<td>".$linha['Identificacao_vac']."</td>";
 			echo "<td>".$linha['Nome_vac']."</td>";
 			echo "<td>".$linha['Raca_vac']."</td>";
-			cho "<td>".$linha['sexo_vac']."</td>";	
+			echo "<td>".$linha['sexo_vac']."</td>";	
 			echo "<td>".$linha['Datanasc_vac']."</td>";
 			echo "<td>".$linha['Racamae_vac']."</td>";
 			echo "<td>".$linha['Racapai_vac']."</td>";
 			echo "<td>".$linha['Altura_vac']."</td>";
-<<<<<<< HEAD
-			echo "<td>".$linha['Altura_vac']."</td>";
-			echo "</tr>";
-=======
 			echo "<td>".$linha['Peso_vac']."</td>";
 		  echo "</tr>";
->>>>>>> 5cc9872535106ab98dfe54ac86b22e7ae89b2e77
 			}
 			echo "</table>";
 		}
