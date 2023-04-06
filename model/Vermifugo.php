d3d6ae391b73bb5780ba74835f0ef7e7cb1da0f3
NATAL� SUELEN DE RAMOS TIMOTEO DA SILVA <nataly.timoteo.silva@escola.pr.gov.br>
2023-04-06 14:38:04 -0300

    a

--- model/Vermifugo.php
+++ model/Vermifugo.php
@@ -180,13 +180,13 @@ function alterarVermifugo($Id_verm,$Nome_verm,$Marca_verm,$Lote_verm,$Fabricacao
 		$sql="update vermifugo_vac set Nome_verm='$Nome_verm', Marca_verm='$Marca_verm', Lote_verm='$Lote_verm', Fabricacao_verm='$Fabricacao_verm', Validade_verm='$Validade_verm', aplicacao_verm='$aplicacao_verm', proximaapli_verm='$proximaapli_verm' where Id_verm=$Id_verm";
 		$resposta=$banco->executar($sql);
 
-=======
+
 		if(!$resposta){
 			return false;
 		}else{
 			return true;
 		}
->>>>>>> cb2bbe733f2456d244813a7efd3e8a2c06c9f4e4
+
 
 
 function acharVermBovino($codigo){
@@ -238,10 +238,7 @@ function acharVermBovino($codigo){
 
 
 
-<<<<<<< HEAD
-=======
 
->>>>>>> cb2bbe733f2456d244813a7efd3e8a2c06c9f4e4
 	function removerVermVac($id){
  	$banco=new Banco();
  	$sql="delete from vermifugo_vac where Id_verm=$id";
@@ -267,8 +264,5 @@ function acharVermBovino($codigo){
 }
 
 
-<<<<<<< HEAD
 
-=======
->>>>>>> cb2bbe733f2456d244813a7efd3e8a2c06c9f4e4
 ?>
