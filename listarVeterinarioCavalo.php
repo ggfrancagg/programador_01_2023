90d4c566b59c7b5207b4e159a56cd386b5af97a0
PriSimon <psimondev@gmail.com>
2023-04-06 15:49:35 -0300

    css listarVet

--- listarVeterinarioCavalo.php
+++ listarVeterinarioCavalo.php
@@ -7,30 +7,29 @@ if(!$consulta){
 	echo "<h5>Não há nenhum Veterinario cadastrado!</h5>";
 }else{
 
-	echo "<table>";
+	echo "<table id=listarbicho>";
 	echo "<tr>";
-	echo "<th>Identificação</th>";
-	echo "<th>Tosa</th>";
-	echo "<th>Nome </th>";
-	echo "<th>Casqueamento</th>";
-	echo "<th>Telefone</th>";
-	echo "<th>Cuidados</th>";	
-	echo "<th>Data de Visita</th>";
-	echo "<th>Cavalo</th>";
+	echo "<th class='ident'>CFMV</th>";
+	echo "<th class='nome'>Nome</th>";	
+	echo "<th class='data'>Telefone</th>";
+	echo "<th class='ident'>Equino</th>";
+	echo "<th class='data'>Data de Visita</th>";
+	echo "<th class='data'>Tosa</th>";
+	echo "<th class='data'>Casqueamento</th>";
+	echo "<th class='nome'>Cuidados</th>";	
 	echo "</tr>";
 
 
 	while ($linha=$consulta->fetch_assoc()){
 		echo "<tr>";
 		echo "<td>".$linha['CFMV']."</td>";
-		echo "<td>".$linha['Tosa_cav']."</td>";	
 		echo "<td>".$linha['Nomevet_cav']."</td>";
-	
-		echo "<td>".$linha['Casqueamento_cav']."</td>";
 		echo "<td>".$linha['Telefonevet_cav']."</td>";
-		echo "<td>".$linha['Cuidados_cav']."</td>";
-		echo "<td>".$linha['Datavisi_cav']."<td>";
 		echo "<td>".$linha['Identificacao_cav']."</td>";
+		echo "<td>".$linha['Datavisi_cav']."</td>";
+		echo "<td>".$linha['Tosa_cav']."</td>";	
+		echo "<td>".$linha['Casqueamento_cav']."</td>";
+		echo "<td>".$linha['Cuidados_cav']."</td>";
 		echo "</tr>";
 	}
 		echo "</table>";
--- listarVeterinarioOvelha.php
+++ listarVeterinarioOvelha.php
@@ -7,28 +7,29 @@ if(!$consulta){
 	echo "<h5>Não há nenhum Veterinario cadastrado!</h5>";
 }else{
 
-	echo "<table>";
-	echo "<tr>";
-	echo "<th>Identificação</th>";
-	echo "<th>Nome</th>";
-	echo "<th>Nascimento</th>";
-	echo "<th>Telefone</th>";
-	echo "<th>Data de Visita</th>";
-	echo "<th>Cuidados</th>";
-	echo "<th>Ovelha</th>";
-	echo "</tr>";
+
+	echo "<table id=listarbicho>";
+		echo "<tr>";
+			echo "<th class='ident'>CFMV</th>";
+			echo "<th class='nome'>Nome</th>";	
+			echo "<th class='data'>Data Nascimento</th>";
+			echo "<th class='data'>Telefone</th>";
+			echo "<th class='ident'>Ovino</th>";
+			echo "<th class='data'>Data de Visita</th>";
+			echo "<th class='nome'>Cuidados</th>";
+		echo "</tr>";
 
 
 	while ($linha=$consulta->fetch_assoc()){
-		echo "<tr>";
-		echo "<td>".$linha['CFMV']."</td>";
-		echo "<td>".$linha['nome_vet']."</td>";
-		echo "<td>".$linha['nasc_vet']."</td>";
-		echo "<td>".$linha['data_visita']."</td>";
-		echo "<td>".$linha['tel_vet']."</td>";
-		echo "<td>".$linha['cuidados_vet']."</td>";
-		echo "<td>".$linha['id_ovl']."</td>";
-		echo "</tr>";
+			echo "<tr>";
+				echo "<td>".$linha['CFMV']."</td>";
+				echo "<td>".$linha['nome_vet']."</td>";
+				echo "<td>".$linha['nasc_vet']."</td>";
+				echo "<td>".$linha['tel_vet']."</td>";
+				echo "<td>".$linha['id_ovl']."</td>";
+				echo "<td>".$linha['data_visita']."</td>";
+				echo "<td>".$linha['cuidados_vet']."</td>";
+			echo "</tr>";
 	}
 		echo "</table>";
 }
