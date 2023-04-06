5b3accc72fb9d616336c467d2966769e0edebdf3
PRISCILA SIMON <simon.2969@aluno.pr.senac.br>
2023-04-06 14:35:20 -0300

    css buscarVet

--- buscarveterinario.php
+++ buscarveterinario.php
@@ -3,17 +3,19 @@
 
 <form action="buscarVeterinario.php" method="GET">
 <h1>Buscar</h1>
-	<p><input type="search" name="busca" placeholder="Nome ou CFMV" required></p>
-	<p><fieldset>
-		<legend>Buscar Veterinário:</legend>
-			
-			<input id="rad" type="radio" name="tipo" value="veterinariocav" required>Veterinario de Equino &#9877;
-			<input id="rad" type="radio" name="tipo" value="veterinariovac" required>Veterinario de Bovino &#9877;
-			<input id="rad" type="radio" name="tipo" value="veterinarioovl" required>Veterinario de Ovino &#9877;
-			
+	
+	<fieldset>
+		<legend><h2>Veterinário:</h2></legend>
+		<p><input type="search" name="busca" placeholder="Nome ou CFMV" required></p>
+			</br>
+			<p><input type="radio" name="tipo" value="veterinariocav" required> Veterinario de Equino </p>
+		  <p><input  type="radio" name="tipo" value="veterinariovac" required> Veterinario de Bovino</p>
+			<p><input  type="radio" name="tipo" value="veterinarioovl" required> Veterinario de Ovino</p>
+			</br>
+			<input id="botbus" type="submit" onclick='mostra()' value="Buscar">
 		</fieldset>
-</p>
-	<h3><input type="submit" onclick='mostra()' value="Buscar"></h3>
+
+	
 </form>
 
 <div id="load">
@@ -37,19 +39,19 @@
 
 			$consulta=buscarVeterinarioCav($busca);
 			if (!$consulta) {
-				echo "<h2>Nenhum Veterinário correspondente!</h2>";
+				echo "<h5>Nenhum Veterinário correspondente!</h5>";
 			}else{
 
-				echo "<table>";
+				echo "<table id='buscaBicho'>";
 				echo "<tr>";
-				echo "<th>Código</th>";
-				echo "<th>Tosa</th>";
-				echo "<th>Nome Veterinario</th>";
-				echo "<th>Casqueamento</th>";
-				echo "<th>Telefone Veterinario</th>";
-				echo "<th>Cuidados</th>";
-				echo "<th>Data da visita</th>";
-				echo "<th>Código cavalo</th>";
+				echo "<th class='ident'>CFMV</th>";
+				echo "<th class='nome'>Nome</th>";
+				echo "<th class='data'>Telefone</th>";
+				echo "<th class='data'>Data da visita</th>";
+				echo "<th class='ident'>Código Equino</th>";
+				echo "<th class='data'>Tosa</th>";
+				echo "<th class='data'>Casqueamento</th>";
+				echo "<th class='nome'>Cuidados</th>";
 				echo "<th class='ident'>Alterar?</th>";
 				echo "<th class='ident'>Remover?</th>";
 				echo "</tr>";
@@ -58,13 +60,13 @@
 		while($linha=$consulta->fetch_assoc()) {
 				echo "<tr>";
 				echo "<td>".$linha['CFMV']."</td>";
-				echo "<td>".$linha['Tosa_cav']."</td>";
 				echo "<td>".$linha['Nomevet_cav']."</td>";
-				echo "<td>".$linha['Casqueamento_cav']."</td>";
 				echo "<td>".$linha['Telefonevet_cav']."</td>";
-				echo "<td>".$linha['Cuidados_cav']."</td>";
 				echo "<td>".$linha['Datavisi_cav']."</td>";
 				echo "<td>".$linha['Identificacao_cav']."</td>";
+				echo "<td>".$linha['Tosa_cav']."</td>";
+				echo "<td>".$linha['Casqueamento_cav']."</td>";
+				echo "<td>".$linha['Cuidados_cav']."</td>";
 				echo "<td><form id='alte' action='alterar.php' method='POST'><input type='hidden' name='CFMVcav' 
 					value='".$linha['CFMV']."'><input id='alt' type='submit' onclick='mostra()' value='sim'></form></td>";
 				echo "<td><form id='alte' action='remover.php' method='POST'><input type='hidden' name='CFMVcav' 
@@ -80,36 +82,33 @@
 
 			$consulta=buscarVeterinarioVac($busca);
 			if (!$consulta) {
-				echo "<h2>Nenhum Veterinário correspondente!</h2>";  	
+				echo "<h5>Nenhum Veterinário correspondente!</h5>";  	
 			}else{
 
-				echo "<table>";
+				echo "<table id='buscaBicho'>";
 				echo "<tr>";
-				echo "<th>Código</th>";
-				echo "<th>Data da visita</th>";
-				echo "<th>Nome Veterinario</th>";
-				echo "<th>Telefone Veterinario</th>";
-				echo "<th>Data de Nascimento</th>";
-				echo "<th>Cuidados</th>";
-				echo "<th>Casqueamento</th>";
-				echo "<th>Código vaca</th>";
+				echo "<th class='ident'>CFMV</th>";
+				echo "<th class='nome'>Nome</th>";
+				echo "<th class='data'>Data da nascimento</th>";
+				echo "<th class='data'>Telefone</th>";
+				echo "<th class='ident'>Código Bovino</th>";
+				echo "<th class='data'>Data da visita</th>";
+				echo "<th class='data'>Casqueamento</th>";
+				echo "<th class='nome'>Cuidados</th>";
 				echo "<th class='ident'>Alterar?</th>";
 				echo "<th class='ident'>Remover?</th>";
 				echo "</tr>";
-				
-			
 
-				
 		while($linha=$consulta->fetch_assoc()) {
 				echo "<tr>";
 				echo "<td>".$linha['CFMV']."</td>";
-				echo "<td>".$linha['Datavisita_vac']."</td>";
 				echo "<td>".$linha['Nomevet_vac']."</td>";
-				echo "<td>".$linha['Telefonevet_vac']."</td>";
 				echo "<td>".$linha['nascvet_vac']."</td>";
-				echo "<td>".$linha['Cuidados_vac']."</td>";
-				echo "<td>".$linha['Casqueamento_vac']."</td>";
+				echo "<td>".$linha['Telefonevet_vac']."</td>";
 				echo "<td>".$linha['Identificacao_vac']."</td>";
+				echo "<td>".$linha['Datavisita_vac']."</td>";
+				echo "<td>".$linha['Casqueamento_vac']."</td>";
+				echo "<td>".$linha['Cuidados_vac']."</td>";
 				echo "<td><form id='alte' action='alterar.php' method='POST'><input type='hidden' name='CFMVvac' 
 					value='".$linha['CFMV']."'><input id='alt' type='submit' onclick='mostra()' value='sim'></form></td>";
 				echo "<td><form id='alte' action='remover.php' method='POST'><input type='hidden' name='CFMVvac' 
@@ -125,20 +124,21 @@
 
 			$consulta=buscarVeterinarioOvl($busca);
 			if (!$consulta) {
-				echo "<h2>Nenhum Veterinário correspondente!</h2>";
+				echo "<h5>Nenhum Veterinário correspondente!</h5>";
 			}else{
 
-				echo "<table>";
+
+				echo "<table id='buscaBicho'>";
 				echo "<tr>";
-				echo "<th>Código</th>";				
-				echo "<th>Nome Veterinario</th>";
-				echo "<th>Nascimento veterinario</th>";
-				echo "<th>Telefone Veterinario</th>";
-				echo "<th>Data da visita</th>";
-				echo "<th>Cuidados</th>";
-				echo "<th>Código Ovelha</th>";
+				echo "<th class='ident'>CFMV</th>";
+				echo "<th class='nome'>Nome</th>";
+				echo "<th class='data'>Data da nascimento</th>";
+				echo "<th class='data'>Telefone</th>";
+				echo "<th class='ident'>Código Ovino</th>";
+				echo "<th class='data'>Data da visita</th>";
+				echo "<th class='nome'>Cuidados</th>";
 				echo "<th class='ident'>Alterar?</th>";
-				echo "<th class='ident'>Remover?</th>"; 
+				echo "<th class='ident'>Remover?</th>";
 				echo "</tr>";
 
 				
@@ -148,9 +148,9 @@
 				echo "<td>".$linha['nome_vet']."</td>";
 				echo "<td>".$linha['nasc_vet']."</td>";
 				echo "<td>".$linha['tel_vet']."</td>";
+				echo "<td>".$linha['id_ovl']."</td>";
 				echo "<td>".$linha['data_visita']."</td>";
 				echo "<td>".$linha['cuidados_vet']."</td>";
-				echo "<td>".$linha['id_ovl']."</td>";
 				echo "<td><form id='alte' action='alterar.php' method='POST'><input type='hidden' name='CFMVovl' 
 					value='".$linha['CFMV']."'><input id='alt' type='submit' onclick='mostra()' value='sim'></form></td>";
 				echo "<td><form id='alte' action='remover.php' method='POST'><input type='hidden' name='CFMVovl' 
